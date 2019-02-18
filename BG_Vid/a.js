80138784cbdabfd9924536d244d8a5d039bbd33f
Thierry <kasskq@gmail.com>
2019-02-18 07:29:29 +0100

    Profile card is nice, bg_vid doesnt work yet

---
+++ BG_Vid/a.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  
+  var vid = $('video');
+  var vid_w_orig = 1280;
+  var vid_h_orig = 720;
+
+  // re-scale image when viewport resizes
+  $(window).resize(function(){
+    
+    // get the parent element size
+    var container_w = vid.parent().width();
+    var container_h = vid.parent().height();
+    
+    // use largest scale factor of horizontal/vertical
+    var scale_w = container_w / vid_w_orig;
+    var scale_h = container_h / vid_h_orig;
+    var scale = scale_w > scale_h ? scale_w : scale_h;
+    
+    // scale the video
+    vid.width(scale * vid_w_orig);
+    vid.height(scale * vid_h_orig);
+    
+  });
+  
+  // trigger re-scale on pageload
+  $(window).trigger('resize');
+  
+});
